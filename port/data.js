afd443e0821308f5e5c91b52e44a5d360a159ab3
Matheus Caldeira <matheuscardozo4@gmail.com>
2020-06-25 17:56:49 -0300

    correcao

---
+++ port/data.js
@@ -0,0 +1,44 @@
+module.exports = [
+  {
+    id: "aCdiL5CzHNo",
+    title: "Bishop Brigs - River",
+    duration: "3:38 min",
+    price: "Free",
+    featured: true
+  },
+  {
+    id: "gpG9QRV9gTk",
+    title: "KALEO - I Can't Go On Without You",
+    duration: "6:19 min",
+    price: "Free",
+    featured: false
+  },
+  {
+    id: "Sv6dMFF_yts",
+    title: "Fun.: We Are Young ft. Janelle Monáe",
+    duration: "4:12 min",
+    price: "Free",
+    featured: false
+  },
+  {
+    id: "8UVNT4wvIGY",
+    title: "Gotye - Somebody That I Used To Know (feat. Kimbra)",
+    duration: "4:03 min",
+    price: "Free",
+    featured: false
+  },
+  {
+    id: "6vopR3ys8Kw",
+    title: "Flume & Chet Faker - Drop the Game",
+    duration: "4:09 min",
+    price: "Free",
+    featured: false
+  },
+  {
+    id: "bWgWKdqoJmo",
+    title: "Tash Sultana - Jungle, extended version",
+    duration: "8:25 min",
+    price: "Free",
+    featured: false
+  },
+]
